b9a886fc834e4d0b93398d1ec34365c19da7a1ea
Vishal Yadav <vishalyadavdeoria274001@gmail.com>
2022-06-10 18:17:00 +0530

    responsive

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -6,41 +6,50 @@ import { WeatherService } from 'src/app/services/weather.service';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  styleUrls: ['./weather.component.css'],
 })
 export class WeatherComponent implements OnInit {
-  constructor(private weatherService: WeatherService,
-    private fb:FormBuilder
-    ) {}
+  constructor(
+    private weatherService: WeatherService,
+    private fb: FormBuilder
+  ) {}
 
   cityName: string = 'Welcome';
   weatherData?: Weather;
-  temperature:number = 20;
-  searchForm:any;
+  temperature: number = 20;
+  searchForm: any;
 
   ngOnInit(): void {
     this.searchForm = this.fb.group({
-    city:['']
-    })
+      city: [''],
+    });
   }
 
   onSubmit() {
-   this.cityName = this.searchForm?.controls.city.value.length < 3 ? 'Enter min 3 characters':this.searchForm?.controls.city.value ;
-   if(this.searchForm?.controls.city.value.length > 3) {this.getWeatherData(this.searchForm?.controls.city.value);}
+    if (this.searchForm?.controls.city.value.length < 3) {
+      this.cityName = 'Enter min 3 char"s"';
+      this.temperature = 0;
+    } else {
+      this.cityName = this.searchForm?.controls.city.value;
+    }
+    if (this.searchForm?.controls.city.value.length > 3) {
+      this.getWeatherData(this.searchForm?.controls.city.value);
+    }
   }
 
   private getWeatherData(cityName: string) {
-  this.weatherService.getWeatherData(cityName)
-  .subscribe(
-    result => {
-      // Handle result
-     this.weatherData = result,
-     this.temperature = result?.temp
-    },
-    error => {
-      this.cityName = error.status === 400 ? 'Please check city name' : 'We are facing issues'
-    },
-  );
+    this.weatherService.getWeatherData(cityName).subscribe(
+      (result) => {
+        // Handle result
+        (this.weatherData = result), (this.temperature = result?.temp);
+      },
+      (error) => {
+        this.cityName =
+          error.status === 400
+            ? 'Please check city name'
+            : 'We are facing issues';
+        this.temperature = 0;
+      }
+    );
   }
-
 }
